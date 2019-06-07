18a44452f5b01f5ee4c098bc8c7a0bc682773827
falkmaxthegodprogrammer <max.r.falk@gmail.com>
2019-06-07 15:35:15 +0200

    -- Jockes testrunda
    Signed-off-by: falkmaxthegodprogrammer <max.r.falk@gmail.com>

--- app/src/main/java/pvt19grupp1/kunskapp/com/kunskapp/util/ConstantKeys.java
+++ app/src/main/java/pvt19grupp1/kunskapp/com/kunskapp/util/ConstantKeys.java
@@ -7,21 +7,21 @@ public class ConstantKeys {
     // STOCKHOLMS STAD API
 
     public static final String API_STHLM_BASE_URL = "http://api.stockholm.se";
-    public static final String API_STHLM_PLACE_KEY = "50ee96a2eed049b0b28c95eeee29d278";
-    public static final String API_STHLM_SERVICEGUIDE_KEY ="e139adc85f4d45aaa67e50091a2da003";
+    public static final String API_STHLM_PLACE_KEY = "";
+    public static final String API_STHLM_SERVICEGUIDE_KEY ="";
 
     // GOOGLE MAPS API
 
-    public static final String API_GOOGLE_MAPS_KEY = "AIzaSyA8AavtO2TeNenIopl64aHSndgADkxJYWM ";
+    public static final String API_GOOGLE_MAPS_KEY = " ";
 
     // GOOGLE PLACES API
 
     public static final String API_GOOGLE_PLACES_BASE_URL = "https://maps.googleapis.com";
-    public static final String API_GOOGLE_PLACES_KEY = "AIzaSyA8AavtO2TeNenIopl64aHSndgADkxJYWM ";
+    public static final String API_GOOGLE_PLACES_KEY = " ";
 
     // GOOGLE DIRECTION API
 
-    public static final String API_GOOGLE_DIRECTIONS_KEY = "AIzaSyBLRD_0Seh4awbkL3EeY8rY4PudnoAsaog";
+    public static final String API_GOOGLE_DIRECTIONS_KEY = ";
 
     //MAP_VIEW_BUNDLE_KEY
     public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
